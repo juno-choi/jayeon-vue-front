b4de5ec33450da8d8028bd6b34cef9f2a77e947f
juno-choi <ililil9482@naver.com>
2022-03-31 15:30:53 +0900

    FEAT : Dockerfile 추가

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  
   devServer:{
+    allowedHosts: "all",
     proxy:{
         "/api":{
-            target: "http://localhost:3000"
+            target: "http://node.jayeonapple.com"
         }
     }
 }  
